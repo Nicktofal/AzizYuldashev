32c7e9861449b2f17c9b449ebe72370faff6f886
Aziz <nicktofal@gmail.com>
2025-05-23 00:30:55 +0300

    x

--- Lab_10/Task_10.3.py
+++ Lab_10/Task_10.3.py
@@ -17,20 +17,16 @@ if holiday not in cards:
 img = Image.open(cards[holiday])
 draw = ImageDraw.Draw(img)
 
-# Настройки текста
 text = name + ", congratulation!"
 font = ImageFont.truetype("ARLRDBD.TTF", 40)
-text_color = (255, 0, 0)  # Красный цвет
+text_color = (255, 0, 0) 
 
-# Рассчитываем позицию текста (внизу по центру)
 text_width = draw.textlength(text, font=font)
 x = (img.width - text_width) / 2
-y = img.height - 100  # Отступ от нижнего края
+y = img.height - 100  
 
-# Добавляем текст на изображение
 draw.text((x, y), text, fill=text_color, font=font)
 
-# Сохраняем и показываем результат
 new_filename = f"congrats_{holiday.replace(' ', '_')}.png"
 img.save(new_filename)
 img.show()
